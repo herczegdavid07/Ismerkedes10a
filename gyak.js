61428a5edf5ae3b1cfb04b41e36166b35db1afb1
herczegdavid07 <herzegdavid@gmail.com>
2023-09-11 15:13:29 +0200

    harmadik kommit

--- gyak.js
+++ gyak.js
@@ -1,6 +1,27 @@
 let test = document.body;
 
-let parameter = document.createlement("p");
-tarto.innerText = "Első paramamétere szövege!";
+let tabla = document.createElement("table");
 
-test.appendChild(parameter);
+let elsoSor = document.createElement("tr");
+
+for (let i = 0; i < 4; i++) {
+  let tableHead = document.createElement("th");
+  tableHead.innerText = `${i}. elem: ${i + 1}`;
+  elsoSor.appendChild(tableHead);
+}
+
+tabla.appendChild(elsoSor);
+
+for (let j = 0; j < 10; j++) {
+  let masodik = document.createElement("tr");
+
+  for (let i = 0; i < 4; i++) {
+    let tableHead = document.createElement("td");
+    tableHead.innerText = `${i}. sor: ${i + 1} elem`;
+    masodik.appendChild(tableHead);
+  }
+
+  tabla.appendChild(masodik);
+}
+
+test.appendChild(tabla);
